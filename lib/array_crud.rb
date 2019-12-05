6c63d53afc2469bbf684f74f2734e3847c54045a
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-05 07:17:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,16 +28,18 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array_exp = ["booty cheeks", "butt cheeks", "coding is life", "lol"]
+  array_exp = ["booty cheeks", "butt cheeks", "am", "lol"]
   array_exp [2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array_exp = ["booty cheeks", "butt cheeks", "coding is life", "lol"]
+  array_exp [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array_exp = ["booty cheeks", "butt cheeks", "coding is life", "lol"]
+  array_exp [-1]
 end
 
 def update_element_from_index(array, index_number, element)
